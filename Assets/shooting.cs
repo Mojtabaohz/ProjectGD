44a9ce2c6a4066e0a7bf299d176b2c0baffcf4a3
MojtabaOHz <moji.ostad@gmail.com>
2020-10-01 13:16:44 +0200

    add a bit more detail to project and finishing shooting
    clean up the code

--- Assets/shooting.cs
+++ Assets/shooting.cs
@@ -8,7 +8,10 @@ public class shooting : MonoBehaviour
     public GameObject bullet;
     public float bulletSpeed = 100f;
  
-
+    public bool shootCD = true;
+    public int shootTimer = 4;
+    protected float Timer;
+    public GameObject ShootSign;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,21 +21,41 @@ public class shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            GameObject TemporaryBullethandler;
-            TemporaryBullethandler = Instantiate(bullet,bulletEmitter.transform.position, bulletEmitter.transform.rotation) as GameObject;
-
-            TemporaryBullethandler.transform.Rotate(Vector3.left * 90);
-
-            Rigidbody TempRigidbody;
-            TempRigidbody = TemporaryBullethandler.GetComponent<Rigidbody>();
-            TempRigidbody.AddForce(transform.forward * bulletSpeed);
-
-
-            Destroy(TemporaryBullethandler, 7.0f);
-           
-           
+        Timer += Time.deltaTime;
+        if(Timer >= shootTimer){
+            Timer = 0;
+            shootCD = true;
+            ShootSign.SetActive(true);
         }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(shootCD){
+                shootCD = false;
+                ShootSign.SetActive(false);
+                GameObject TemporaryBullethandler;
+                TemporaryBullethandler = Instantiate(bullet,bulletEmitter.transform.position, bulletEmitter.transform.rotation) as GameObject;
+
+                TemporaryBullethandler.transform.Rotate(Vector3.left * 90);
+
+                Rigidbody TempRigidbody;
+                TempRigidbody = TemporaryBullethandler.GetComponent<Rigidbody>();
+                TempRigidbody.AddForce(transform.forward * bulletSpeed);
+
+                Destroy(TemporaryBullethandler, 7.0f);
+                
+            }
+            else{
+
+                print("shooting is on cooldown");
+            }
+        }
+        else{
+
+
+        }
+            
+           
+           
+        
     }
    
 }
