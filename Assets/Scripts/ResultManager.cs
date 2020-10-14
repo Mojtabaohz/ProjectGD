776f52b9fe916bb047a7b47b89a00dc656abb3ac
MojtabaOHz <moji.ostad@gmail.com>
2020-10-14 18:43:03 +0200

    Add new scene and winner screen.

---
+++ Assets/Scripts/ResultManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ResultManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player01Base;
+    public GameObject player02Base;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player01Base.GetComponent<HealthBar>().alive == false){
+            Debug.Log("base has been destroyed");
+            Manager.Instance.result.text = "Red Wins";
+            SceneManager.LoadScene("ResultScene");
+        }else if(player02Base.GetComponent<HealthBar>().alive == false){
+            Manager.Instance.result.text = "Blue Wins";
+            SceneManager.LoadScene("ResultScene");
+        }
+        
+    }
+}
---
+++ Assets/Scripts/SetText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SetText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text result;
+    void Start()
+    {
+        result.GetComponent<Text>().text = Manager.Instance.result.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetWinnerText(){
+    }
+}
